ee1cc9fdb658c409c0efa2d96fb7a97394c0e076
Isaac Kelly <isaacmkelly0@gmail.com>
2021-03-19 17:36:58 -0500

    cipher passes all tests

--- cipher.js
+++ cipher.js
@@ -1,4 +1,21 @@
 const cipher = input => {
-    return "data";
+    let output = ''
+    let amount = 3
+    for(let i = 0; i < input.length; i++){
+        let c = input[i];
+        if(c.match(/[a-z]/i)){
+            let code = input.charCodeAt(i)
+            //uppercase letters
+            if(code >= 65 && code <= 90){
+                c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+            }
+            //lowercase letters
+            else if(code >= 97 && code <= 122){
+                c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+            }
+        }
+        output += c
+    }
+    return output;
 }
 module.exports = cipher;
--- cipher.test.js
+++ cipher.test.js
@@ -1,4 +1,13 @@
 const cipher = require('./cipher');
-test("something something cipher", ()=>{
-    expect(cipher("string")).toBe("data");
+test("test if cipher handles basic chars", ()=>{
+    expect(cipher("abc")).toEqual("def");
+})
+test("test case sensitivity", ()=>{
+    expect(cipher("DEF")).toBe("GHI");
+})
+test("test wrapping from z to a", ()=>{
+    expect(cipher("Z")).toBe("C");
+})
+test("test punctuation", ()=>{
+    expect(cipher(",;&*!3a")).toEqual(",;&*!3d");
 })

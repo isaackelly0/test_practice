60d556dfb5c3ec3b8c8704b0f01df4c0589617b6
Isaac Kelly <isaacmkelly0@gmail.com>
2021-03-19 18:24:45 -0500

    analyze funtion returns an object with notes on array qualities

--- analyze.js
+++ analyze.js
@@ -1,4 +1,26 @@
 const analyze = input => {
-    return "data"
+    let min;
+    let max;
+    let total = 0;
+    for(let i = 0; i < input.length; i++){
+        if(min && max){
+            if(min > input[i]){
+                min = input[i];
+            }else if(max < input[i]){
+                max = input[i];
+            }
+        }else{
+            min = input[i];
+            max = input[i];
+        }
+        total += input[i];
+    }
+    let average = total / input.length;
+    return {
+        average: average,
+        min: min,
+        max: max,
+        length: input.length
+    }
 }
 module.exports = analyze;
--- analyze.test.js
+++ analyze.test.js
@@ -1,4 +1,28 @@
 const analyze = require('./analyze');
-test("blahblahblah", ()=>{
-    expect(analyze("string")).toBe("data")
+test("Analysis of a number array's qualities", ()=>{
+    const object = analyze([1,2,3,4,5]);
+    object == {
+        average: 3,
+        min: 1,
+        max: 5,
+        length: 5
+    }
+})
+test("Let's try with negatives", ()=>{
+    const object = analyze([-5,-10,-15]);
+    object == {
+        average: -10,
+        min: -15,
+        max: -5,
+        length: 3
+    }
+})
+test("Variety of numbers this time", ()=>{
+    const object = analyze([4, 76, 90, 100, 2, 12, 555, -2200, -1]);
+    object == {
+        average: -151.3,
+        min: -2200,
+        max: 555,
+        length: 9
+    }
 })
